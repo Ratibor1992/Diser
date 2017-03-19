8e26e22dfd0e94e6613fe20de8c9e1c420c31154
Ratibor1992 <chopey.ratybor@gmail.com>
2017-03-19 15:42:58 +0200

    - add read list of user folders and files

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -9,6 +9,8 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
 
+
+
 namespace WindowsFormsApplication1
 {
     public partial class Form1 : Form
@@ -30,17 +32,85 @@ namespace WindowsFormsApplication1
         private void button2_Click(object sender, EventArgs e)
         {
             //// add find map. file 
-            string MapFilepath;
-            string ReportPath = @"D:\Report.txt";
-            string[] lines;
+            Parser.GetFolderUserFolder(folderBrowserDialog1.SelectedPath);
 
-            var allFiles = Directory.GetFiles(folderBrowserDialog1.SelectedPath, "*.map", SearchOption.AllDirectories);
-            MapFilepath = allFiles[0];
-            //// parser
-            lines = System.IO.File.ReadAllLines(MapFilepath);
-            File.Create(ReportPath);
-            ///new commit
-
+            /*       var allFiles = Directory.GetFiles(folderBrowserDialog1.SelectedPath, "*.map", SearchOption.AllDirectories);
+                   MapFilepath = allFiles[0];
+                   //// parser
+                   lines = System.IO.File.ReadAllLines(MapFilepath);
+                   FileStream file1 = new FileStream(ReportPath, FileMode.Create);
+                   StreamWriter writer = new StreamWriter(file1);
+                   //File.Create(ReportPath);
+                   ///new commit
+                   ///
+                   foreach (string line in lines)
+                   {
+                       if ((line.Contains("Thumb Code")) && (line.Contains("(i.")))
+                       {
+                           writer.Write(line);
+                           writer.Write("\r\n");
+                       }
+                   }
+                   writer.Close();*/
         }
     }
+}
+
+
+
+class Parser
+{
+    public const string SVN = "svn";
+    public const string BINOUTPUT = "BINOUTPUT";
+    public const string Objects = "Objects";
+    public const string Listings = "Listings";
+    public const string RTE = "RTE";
+
+
+    public static void GetFolderUserFolder(string Path)
+    {
+        string ReportPath = @"D:\Report2.txt";
+
+        List<string> folders = new List<string>(Directory.EnumerateDirectories(Path));
+        //UserFolderList<string> = Directory.EnumerateDirectories(folderBrowserDialog1.SelectedPath);
+        // add remove special folder
+        FileStream file1 = new FileStream(ReportPath, FileMode.Create);
+        StreamWriter writer = new StreamWriter(file1);
+
+        foreach (string line in folders)
+        {
+            if ((line.Contains(SVN)) ||
+                (line.Contains(BINOUTPUT)) ||
+                (line.Contains(Objects)) ||
+                (line.Contains(Listings)) ||
+                (line.Contains(RTE)))
+            {
+
+            }
+            else
+            {
+                writer.Write(line);
+                writer.Write("\r\n");
+                string[] dirs = Directory.GetFiles(line, "*.c");
+                writer.Write("\r\n");
+                foreach (string dir in dirs)
+                {
+                    writer.Write(dir);
+                    writer.Write("\r\n");
+                    //Console.WriteLine();
+                }
+            }
+        }
+        writer.Close();
+    }
+   /* public static void GetFiliList(string Upath, string type)
+    {
+        string[] dirs = Directory.GetFiles(Upath, type);
+        foreach (string dir in dirs)
+        {
+            writer.Write(dir);
+            //Console.WriteLine();
+        }
+    }*/
+
 }
