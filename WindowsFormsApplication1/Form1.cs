fa5fed36a4bb8481ec2736962267b2b00000ee8c
Ratibor1992 <chopey.ratybor@gmail.com>
2017-03-23 23:10:16 +0200

    -add few modification

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -10,11 +10,13 @@ using System.Windows.Forms;
 using System.IO;
 
 
-
 namespace WindowsFormsApplication1
 {
     public partial class Form1 : Form
     {
+        public string ReportPath;
+        
+
         public Form1()
         {
             InitializeComponent();
@@ -32,8 +34,8 @@ namespace WindowsFormsApplication1
         private void button2_Click(object sender, EventArgs e)
         {
             //// add find map. file 
-            Parser.GetFolderUserFolder(folderBrowserDialog1.SelectedPath);
-
+            Parser.GetFolderFileName(folderBrowserDialog1.SelectedPath, ReportPath);    
+            label3.Text = "Result: OK";
             /*       var allFiles = Directory.GetFiles(folderBrowserDialog1.SelectedPath, "*.map", SearchOption.AllDirectories);
                    MapFilepath = allFiles[0];
                    //// parser
@@ -53,6 +55,16 @@ namespace WindowsFormsApplication1
                    }
                    writer.Close();*/
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*";
+            saveFileDialog1.Title = "Save an Text File";
+            saveFileDialog1.ShowDialog();
+           
+            textBox2.Text = ReportPath = saveFileDialog1.FileName; ;
+        }
     }
 }
 
@@ -65,52 +77,46 @@ class Parser
     public const string Objects = "Objects";
     public const string Listings = "Listings";
     public const string RTE = "RTE";
+    public string[] FileInProject;
 
 
-    public static void GetFolderUserFolder(string Path)
+    public static void GetFolderFileName(string Project, string Report)
     {
-        string ReportPath = @"D:\Report2.txt";
-
-        List<string> folders = new List<string>(Directory.EnumerateDirectories(Path));
-        //UserFolderList<string> = Directory.EnumerateDirectories(folderBrowserDialog1.SelectedPath);
-        // add remove special folder
-        FileStream file1 = new FileStream(ReportPath, FileMode.Create);
+        List<string> folders = new List<string>(Directory.EnumerateDirectories(Project));
+        FileStream file1 = new FileStream(Report, FileMode.Create);
         StreamWriter writer = new StreamWriter(file1);
 
         foreach (string line in folders)
         {
             if ((line.Contains(SVN)) ||
                 (line.Contains(BINOUTPUT)) ||
-                (line.Contains(Objects)) ||
-                (line.Contains(Listings)) ||
+                (line.Contains(Objects))   ||
+                (line.Contains(Listings))  ||
                 (line.Contains(RTE)))
             {
 
             }
             else
             {
-                writer.Write(line);
+                string Tmp1;
+
+                Tmp1 = line.Replace(Project, "");
+                Tmp1 = Tmp1.Replace(@"\", "");
+                writer.Write(Tmp1);
                 writer.Write("\r\n");
                 string[] dirs = Directory.GetFiles(line, "*.c");
-                writer.Write("\r\n");
                 foreach (string dir in dirs)
                 {
-                    writer.Write(dir);
+                    string Tmp;
+                    Tmp = dir.Replace(line, "");
+                    Tmp = Tmp.Replace(@"\","");
+                    writer.Write("\t");
+                    writer.Write(Tmp);
                     writer.Write("\r\n");
-                    //Console.WriteLine();
                 }
+                writer.Write("\r\n");
             }
         }
         writer.Close();
     }
-   /* public static void GetFiliList(string Upath, string type)
-    {
-        string[] dirs = Directory.GetFiles(Upath, type);
-        foreach (string dir in dirs)
-        {
-            writer.Write(dir);
-            //Console.WriteLine();
-        }
-    }*/
-
 }
