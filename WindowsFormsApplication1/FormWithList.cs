12617c1ef8592801d24b773aa881f1c1772fde3e
Ratibor1992 <chopey.ratybor@gmail.com>
2017-04-07 18:41:57 +0300

    -fixed issue with prev. commite

---
+++ WindowsFormsApplication1/FormWithList.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsApplication1;
+
+
+namespace WindowsFormsApplication1
+{
+    public partial class FormWithList : Form
+    {
+        private Project _project;
+
+
+        public FormWithList(Project project)
+        {
+            _project = project;
+            InitializeComponent();
+
+            foreach (var func in _project.Functions)
+            {
+                FunctionListBox.Items.Add(func.Name);
+            }
+        }
+    }
+}
